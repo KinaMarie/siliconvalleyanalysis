0a873d8a1be32c34c632041155759f042c67c14b
C Zhang <zhang.chuck@gmail.com>
2015-11-15 01:33:53 -0800

    added code to ensure bipartiteness; kept the larger of in-degree or out-degree

--- code/0_cleaning/2_create_max_wcc.py
+++ code/0_cleaning/2_create_max_wcc.py
@@ -32,6 +32,21 @@ def customGraphGenerator(filename, attribute_key):
 # Generate the graph
 g = customGraphGenerator(input_file, attribute_key = attributes)
 
+# Ensure bipartiteness
+# If a node has both incoming edges and outgoing edges, keep the larger set of edges.
+# i.e., 8 in-degree, 4 out-degree --> discard all outgoing edges for the node
+in_d = g.in_degree()
+out_d = g.out_degree()
+
+for node in g.nodes_iter():
+    if in_d[node] != 0 and out_d[node] != 0:
+        if in_d[node] > out_d[node]:
+        	for destination in g.successors(node):
+        		g.remove_edge(node, destination)
+        else:
+        	for source in g.predecessors(node):
+        		g.remove_edge(source, node)
+
 # Calculate the MaxWCC
 maxWCCnodes = max(nx.weakly_connected_components(g), key=len)
 wcc = g.subgraph(maxWCCnodes)
@@ -40,4 +55,4 @@ wcc = g.subgraph(maxWCCnodes)
 nx.write_edgelist(wcc, output_file, delimiter="\t", data=True)
 
 ## READ IN USING THIS FUNCTION
-# g = nx.read_edgelist("test.edgelist", delimiter="\t", data=True)
+# g = nx.read_edgelist("max_wcc_graph.edgelist", create_using=nx.DiGraph(), delimiter="\t", data=True)
